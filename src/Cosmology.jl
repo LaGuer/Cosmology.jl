581dfc37d0b5ddfb32945d026851c84fefc74a8c
Laurent G. OSSBSS AMIS UCC <27735554+LaGuer@users.noreply.github.com>
2019-04-25 05:27:16 -0700

    Update Cosmology.jl

--- src/Cosmology.jl
+++ src/Cosmology.jl
@@ -102,7 +102,7 @@ function a2E(c::Union{FlatWCDM,ClosedWCDM,OpenWCDM}, a)
     sqrt(c.Ω_r + (c.Ω_m + c.Ω_k*a)*a + c.Ω_Λ*ade)
 end
 
-function cosmology(;h=0.69,
+function cosmology(;h=0.7,
                    Neff=3.04,
                    OmegaK=0,
                    OmegaM=0.29,
